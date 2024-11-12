06f026dcb4b544a80837973378c36d7479a4f144
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-11-12 17:00:00 -0300

    fedeslab

--- frontend/src/components/Video/VideoWistia.jsx
+++ frontend/src/components/Video/VideoWistia.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import AnimatedButton from "../AnimatedButton/AnimatedButton";
 import "./Video.css";
-
+import Registro from "../Registro/Registro";
 const VideoWistia = ({}) => {
+  const [showForm, setShowform] = useState(false);
   const videoUrl = "https://fast.wistia.net/embed/iframe/5lx6fgn9l7";
 
+  const actualizarEstadoPadre = (estado) => {
+    setShowform(estado);
+  };
+  
+  const handleClick = (click) => {
+    setShowform(click);
+  };
   return (
     <div className="w-full h-full flex flex-col items-center justify-center  px-0 lg:px-32">
       <div className="w-full lg:w-[60%] h-full flex flex-col items-center px-1 lg:px-6 py-1 lg:py-4 bg-gray-800 rounded-2xl mb-4 border-gray-800 border-2 relative">
@@ -26,8 +34,24 @@ const VideoWistia = ({}) => {
         tus ventas online en menos de 3 meses.❜
       </p>
       <div className="w-full flex justify-center">
-        <AnimatedButton />
+        <AnimatedButton  actualizarEstado={actualizarEstadoPadre} />
       </div>
+      {showForm && (
+        <>
+          <div
+            className="fixed inset-0 bg-gray-800 opacity-50 z-40"
+            onClick={() => handleClick(false)}
+          ></div>
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white p-4 rounded-lg shadow-lg">
+              <Registro
+                actualizarEstado={actualizarEstadoPadre}
+      
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
